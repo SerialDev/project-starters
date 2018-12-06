54b5f69adbaf6fe201c1b75351697f1d6b48eb9f
Andres Mariscal <carlos.mariscal.melgar@gmail.com>
2018-12-06 16:16:48 +0000

    [+] fn init() ; fn render();

--- main.cpp
+++ main.cpp
@@ -1,27 +1,32 @@
+
 #include<iostream>
 #include<SDL2/SDL.h>
 
 SDL_Window* g_pWindow = 0;
 SDL_Renderer* g_pRenderer = 0;
 
-int main(int argc, char **argv){
-
-    if (SDL_Init(SDL_INIT_EVERYTHING) != 0){
-        std::cout << "Error while initializing SDL: " << SDL_GetError() << std::endl;
-        SDL_Quit();
-        return -1;
-    }
-    std::cout << "SDL Initialized " << std::endl;
-
-    g_pWindow = SDL_CreateWindow("Setting up",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,640,480, SDL_WINDOW_SHOWN);
-
-    if (g_pWindow != 0)
+bool init(const char* title, int xpos, int ypos, int height, int width, int flags)
+{
+    if (SDL_Init(SDL_INIT_EVERYTHING) >= 0)
     {
-        g_pRenderer = SDL_CreateRenderer(g_pWindow, -1,0);
+        // if succeeded create our window
+        g_pWindow = SDL_CreateWindow(title,xpos,ypos, height,width,flags);
+        if (g_pWindow != 0) {
+            g_pRenderer = SDL_CreateRenderer(g_pWindow,-1,0);
+        }
+    }
+    else
+    {
+        std::cout << "Failed to init SDL" << std::endl;
+        return false; // sdl failed to initialize
     }
 
-    // everything succeeded lets draw the window
-    // set to black
+    return true;
+}
+
+void render()
+{
+    // set to Black
     SDL_SetRenderDrawColor(g_pRenderer,0,0,0,255);
 
     // clear window to black
@@ -29,11 +34,27 @@ int main(int argc, char **argv){
 
     // Show the window
     SDL_RenderPresent(g_pRenderer);
+}
+
+int main(int argc, char **argv){
+    bool g_bRunning = false; // This will create a loop
+
+
+    if (init("SDL_INIT", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 640, SDL_WINDOW_SHOWN)) {
+        g_bRunning= true;
+    }
+    else{
+        return 1; 
+    }
+
+    while (g_bRunning) {
+        render();
+    }
 
     // set a delay before quitting
     SDL_Delay(5000);
-    
-    // Clean up 
+
+    // Clean up
     SDL_Quit();
 
     return 0;
